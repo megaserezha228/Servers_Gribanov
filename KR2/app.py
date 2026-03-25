2929b57cd88466b0ad32ac0716d8d39990c636fd
megaserezha228 <docing101@yandex.ru>
2026-03-25 18:46:30 +0300

    Update app.py

--- KR2/app.py
+++ KR2/app.py
@@ -30,11 +30,11 @@ async def create_user(user: UserCreate):
 
 
 sample_products = [
-    {"product_id": 123, "name": "Smartphone", "category": "Electronics", "price": 599.99},
-    {"product_id": 456, "name": "Phone Case", "category": "Accessories", "price": 19.99},
-    {"product_id": 789, "name": "Iphone", "category": "Electronics", "price": 1299.99},
-    {"product_id": 101, "name": "Headphones", "category": "Accessories", "price": 99.99},
-    {"product_id": 202, "name": "Smartwatch", "category": "Electronics", "price": 299.99}
+    {"product_id": 123, "name": "Smartphone", "category": "Electronics", "price": 60000},
+    {"product_id": 456, "name": "Phone Case", "category": "Accessories", "price": 2000},
+    {"product_id": 789, "name": "Iphone", "category": "Electronics", "price": 130000},
+    {"product_id": 101, "name": "Headphones", "category": "Accessories", "price": 10000},
+    {"product_id": 202, "name": "Smartwatch", "category": "Electronics", "price": 30000}
 ]
 
 
